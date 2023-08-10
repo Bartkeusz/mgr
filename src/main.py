89dcf182b33b4e96d0e8aa771f6e0d74315185fe
Bartkeusz <kletkiewiczbartlomiej@gmail.com>
2023-08-11 00:13:46 +0200

    added vgg

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 import numpy as np
 import mlflow
+import json
 from urllib3.exceptions import NewConnectionError
 
 from read_data import read_data, prepare_dataset
@@ -11,23 +12,31 @@ import models
 from visualisation import plot_history
 
 
-
-def run() -> dict:
+def run(path_to_config: str = "read_data_config.json") -> None:
+    with open(path_to_config, 'r') as f:
+        config = json.load(f)
+    
     labels, number_of_classes = read_data()
     x, y = prepare_dataset(labels)
     x_train, x_test, y_train, y_test = split_data_into_train_and_test(x, y, number_of_classes)
 
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    mlflow.set_experiment("pierwsze uruchomienie")
-    experiment = mlflow.get_experiment_by_name("pierwsze uruchomienie")
-    with mlflow.start_run(run_name=f"test_run", experiment_id=experiment.experiment_id):
+    mlflow.set_experiment("test1")
+
+    experiment = mlflow.get_experiment_by_name("test1")
+
+    with mlflow.start_run(run_name=f"test_run_2", experiment_id=experiment.experiment_id):
         mlflow.tensorflow.autolog()
-        
-        model = models.UseResNet50model(number_of_classes, 16, [224, 224], x[0].shape, 3)
+        model = models.UseResNet50model(number_of_classes, config['batch_size'], config['image_size'], x[0].shape, config['patience'], config['optimizer'])
         model.build_model()
         history = model.train_model(x_train, y_train, x_test, y_test)
-        print(history)
-        plot_history(history)
+
+    # with mlflow.start_run(run_name=f"test_run_3", experiment_id=experiment.experiment_id):
+    #     mlflow.tensorflow.autolog()
+    #     model = models.UseVGGmodel(number_of_classes, config['batch_size'], config['image_size'], x[0].shape, config['patience'], config['optimizer'])
+    #     model.build_model()
+    #     history, model_accuracy = model.train_model(x_train, y_train, x_test, y_test)
+
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -5,18 +5,37 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, models
+from tensorflow.keras.metrics import CategoricalAccuracy
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import EarlyStopping
+from keras import backend as K
 
-from visualisation import show_classification_report
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 class UseResNet50model:
-    def __init__(self, number_of_classes, batch_size, img_size, dim, patience):
+    def __init__(self, number_of_classes, batch_size, img_size, dim, patience, optimizer):
         self.number_of_classes = number_of_classes
         self.batch_size = batch_size
         self.img_size = img_size
         self.dim = dim
         self.patience = patience
+        self.optimizer = optimizer
 
     def build_model(self):
         self.model = ResNet50(
@@ -33,6 +52,59 @@ class UseResNet50model:
 
         self.model = models.Sequential([
             self.model,
+            # Conv2D(32, (3,3), input_shape=(self.img_size, self.number_of_classes), activation='relu', padding='same'),
+            # Conv2D(32, (3,3), activation='relu', padding='same'),
+            # MaxPooling2D(2,2),
+            # Conv2D(64, (3,3), activation='relu', padding='same'),
+            # Conv2D(64, (3,3), activation='relu', padding='same'),
+            # MaxPooling2D(2,2),
+            # Conv2D(128, (3, 3), activation = 'relu', padding='same'),
+            flatten_layer,
+            prediction_layer
+            ])
+        
+
+        
+        self.model.summary()
+
+        self.model.compile(
+          optimizer=self.optimizer,
+          loss=['categorical_crossentropy','mse'],
+          metrics=['accuracy', 'CategoricalAccuracy']
+          )
+
+    def train_model(self, x_train, y_train, x_test, y_test):
+        es = EarlyStopping(monitor='val_accuracy', mode='max', patience=self.patience, restore_best_weights=True)
+        self.history = self.model.fit(x_train, y_train, epochs=100, validation_split=0.1, batch_size=self.batch_size, callbacks=[es])
+
+        model_accuracy = self.model.evaluate(x_test, y_test, batch_size=self.batch_size, return_dict=True)
+
+        return self.history, model_accuracy
+    
+
+class UseVGGmodel:
+    def __init__(self, number_of_classes, batch_size, img_size, dim, patience, optimizer):
+        self.number_of_classes = number_of_classes
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.dim = dim
+        self.patience = patience
+        self.optimizer = optimizer
+
+    def build_model(self):
+        self.model =  VGG16(
+        include_top=False,
+        weights="imagenet",
+        classifier_activation="relu",
+        input_shape = self.dim,)
+
+        self.model.trainable = False
+
+        flatten_layer = layers.Flatten()
+        prediction_layer = layers.Dense(self.number_of_classes, activation='softmax')
+
+        self.model = models.Sequential([
+            self.model,
             flatten_layer,
             prediction_layer
             ])
@@ -40,7 +112,7 @@ class UseResNet50model:
         self.model.summary()
 
         self.model.compile(
-          optimizer='adam',
+          optimizer=self.optimizer,
           loss='categorical_crossentropy',
           metrics=['accuracy']
           )
@@ -50,8 +122,5 @@ class UseResNet50model:
         self.history = self.model.fit(x_train, y_train, epochs=100, validation_split=0.1, batch_size=self.batch_size, callbacks=[es])
 
         model_accuracy = self.model.evaluate(x_test, y_test, batch_size=self.batch_size, return_dict=True)
-        print(f"ResNet50 model accuracy: {model_accuracy}")
-        print(show_classification_report(y_test, x_test, self.model))
 
-        return self.history
-    
+        return self.history, model_accuracy
